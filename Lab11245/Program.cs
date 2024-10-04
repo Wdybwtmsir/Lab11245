0f8948c0f17b5695344a10d728dfd3f964bd7514
khrom <khrom@DESKTOP-DA2UCIR>
2024-10-04 18:38:16 +0200

    Добавьте файлы проекта.

---
+++ Lab11245/Program.cs
@@ -0,0 +1,14 @@
+﻿try
+{
+    Console.Write("Введите целое число N: ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    bool conditionA = (N % 4 == 0) || (N % 7 == 0);
+    bool conditionB = (N % 5 == 0) && (N % 10 != 0);
+
+    Console.WriteLine($"Условие (а): {conditionA}");
+    Console.WriteLine($"Условие (б): {conditionB}");
+}
+catch
+{
+    Console.WriteLine("Введите корректные данные");
+}
